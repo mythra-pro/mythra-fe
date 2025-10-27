55a62980186e67d851295945c4c7a5ab562ac9e6
ahmad rizki maulana <ahmadrizkimaulana666@gmail.com>
2025-10-27 14:04:21 +0700

    fix the build from Raka

--- app/dashboard/staff/checkin/page.tsx
+++ app/dashboard/staff/checkin/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect } from "react";
 import { DashboardLayout } from "@/components/dashboard-layout";
 import { getMenuSectionsForRole } from "@/app/utils/dashboardMenus";
